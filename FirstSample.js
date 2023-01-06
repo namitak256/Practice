d095ab70560f28ab272214f72b1ddf6544419aec
namitak256 <106014428+namitak256@users.noreply.github.com>
2023-01-06 19:53:04 +0530

    Add files via upload

---
+++ FirstSample.js
@@ -0,0 +1,14 @@
+
+(function(){
+    let input = document.getElementsByClassName('gLFyf');
+    for(let i=0; i<input.length;i++){
+    input[i].innerHTML = "namita";
+    }
+})();
+
+javascript:(function(){
+    let script = document.createElement('script');
+    script.src = 'https://cdn.jsdelivr.net/gh/namitak256/Practice/FirstSample.js';
+    document.body.appendChild(script);})()
+
+javascript:(function(){let script = document.createElement('script');script.src = 'https://cdn.jsdelivr.net/gh/namitak256/Practice/FirstSample.js';document.body.appendChild(script);})()
