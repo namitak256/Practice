5bb67b045f238a9d36b04e75c1721480dcd6e06f
namitak256 <106014428+namitak256@users.noreply.github.com>
2023-01-06 19:26:16 +0530

    Update FirstSample.js

--- FirstSample.js
+++ FirstSample.js
@@ -1,8 +1,12 @@
 
 (function(){
-    let input = document.getElementsByClassName('gLFyf');
+    let input = document.getElementsByName('firstname');
     for(let i=0; i<input.length;i++){
-    input[i].innerHTML = "namita";
+    input[i].value = "namita";
+    }
+    let input = document.getElementsByName('lastname');
+    for(let i=0; i<input.length;i++){
+    input[i].value = "shukla";
     }
 })();
 
