81c43771bc95fe749b32f2e41ff5909a55c095f9
namitak256 <106014428+namitak256@users.noreply.github.com>
2023-01-08 17:47:28 +0530

    Add files via upload

---
+++ Sample.js
@@ -0,0 +1,10 @@
+(function(){
+    let input1 = document.getElementsByName('firstname');
+    for(let i=0; i<input1.length;i++){
+    input1[i].value = "namita";
+    }
+    let input2 = document.getElementsByName('lastname');
+    for(let i=0; i<input2.length;i++){
+    input2[i].value = "shukla";
+    }
+})();
